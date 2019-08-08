a4f5f2375fc4e0677f5f8b400ad7679775d3edcc
sonny_maan@yahoo.com <ubuntu@ip-172-31-17-147.eu-west-1.compute.internal>
2019-08-08 12:26:50 +0000

    added random name generator for the kitty

--- app.rb
+++ app.rb
@@ -3,3 +3,17 @@ require 'sinatra'
 get '/' do
   "Hello World! Welcome to Ruby!"
 end
+
+get '/secret' do
+    "my secret is that I think Queen's music isnt even good tbh"
+end
+
+get '/why-queen-is-shit' do
+    "OverHyped"
+end
+
+get '/cat' do
+  erb(:index)
+end
+
+set :session_secret, 'super secret'
