b1e9c258fdcbec279d73ec59cf036b9e8d313104
sonny_maan@yahoo.com <ubuntu@ip-172-31-17-147.eu-west-1.compute.internal>
2019-08-08 13:23:57 +0000

    added a named cats route which displays the query string as a kitty name Co Authored by: Chuan Cheng <c_chuan23@hotmail.com>

--- app.rb
+++ app.rb
@@ -1,21 +1,15 @@
-require 'sinatra'
+require 'sinatra' 
 
-get '/' do
-  "Hello World! Welcome to Ruby!"
-end
-
-get '/secret' do
-    "my secret is that I think Queen's music isnt even good tbh"
-end
-
-get '/why-queen-is-shit' do
-    "OverHyped"
-end
-
-get '/cat' do
+get '/random-cat' do
   @kittyname = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
+
+get '/named-cat' do
+  @kittyname = params[:name]
+  erb(:index)
+end
+
 
 
 set :session_secret, 'super secret'
